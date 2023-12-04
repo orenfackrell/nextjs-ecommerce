067539e57ddd63203b99497e504bcb9f9f3a1b62
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-12-04 21:22:59 +0000

    make page responsive to smaller screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased w-screen h-screen m-auto px-4`}>
+      <body className={`${inter.className} antialiased w-fit h-screen m-auto md:px-4`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,17 @@ import { NavMenu } from './components/navbar';
 import { UserIcons } from './components/user-icons';
 import { Footer } from './components/footer';
 import { Hero } from './components/hero';
+import { NavSheet } from './components/navsheet';
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-between">
-      <nav className="flex justify-between border-b p-2 mb-2">
+    <main className="h-screen flex flex-col md:transition-all">
+      <nav className="hidden md:flex justify-between border-b p-2 mb-2">
         <NavMenu />
+        <UserIcons />
+      </nav>
+      <nav className="flex md:hidden justify-between border-b p-2 mb-2">
+        <NavSheet />
         <UserIcons />
       </nav>
 
