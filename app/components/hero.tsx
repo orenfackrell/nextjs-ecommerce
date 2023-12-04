22dc2cdb99c638f7151b701041a0eab915dc8d3a
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-12-04 21:20:03 +0000

    make hero responsive for smaller screens

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export function Hero() {
   return (
-    <section className="flex items-center justify-between w-full">
+    <section className="flex-col flex items-center justify-between w-full my-4 md:h-1/3 md:flex-row p-2">
       <div className="flex flex-col items-start justify-center w-full h-full p-6 space-y-6">
         <h1 className="text-4xl font-bold">Terrarium Emporium</h1>
         <p className="text-lg">
@@ -12,13 +12,15 @@ export function Hero() {
         </p>
         <a href="/" />
       </div>
-      <Image
-        className="place-self-end"
-        src="/hero-desktop.jpg"
-        width={720}
-        height={760}
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+      <div className="relative w-full max-w-2xl h-full aspect-video">
+        <Image
+          className="place-self-end"
+          src="/hero-desktop.jpg"
+          quality={100}
+          fill
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
     </section>
   );
 }
